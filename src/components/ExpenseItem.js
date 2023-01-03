74316cb29982d9e0fbaaec27a78861e34ed33aa1
Prashanth-Reddy19 <kotha.prashanth02>
2023-01-03 14:10:53 +0530

    sample program

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+function ExpenseItem() {
+    return (
+
+        <div>
+            <div>
+                <h2>ExpenseItems</h2>
+
+
+                <div>Food Rs 10</div>
+                <div>Petrol Rs 100</div>
+                <div>Movie Rs 200</div>
+                
+            </div>
+        </div>
+    );
+
+}
+export default ExpenseItem;
