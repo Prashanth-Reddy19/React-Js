2aa11a43aeb4ab48f2412f36f4c756fb89f07cb8
Prashanth-Reddy19 <kotha.prashanth02>
2023-01-05 20:53:44 +0530

    change expense button

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,26 +1,31 @@
-
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import './Expenseitem.css';
 
 function ExpenseItem(props) {
+    const [title, setTitle] = useState(props.amount)
 
-    const clickHandler= () =>{
-        console.log('Deleted');
+
+
+    const clickHandler = () => {
+        setTitle('$100')
+        console.log(setTitle)
+
     }
-    
+
 
     return (
         <div className='expense-item'>
             <ExpenseDate date={props.date} />
-           
+
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Delete Expense</button>
-            
-           
-        
+            <button onClick={clickHandler}>Change Expenses</button>
+
+
+
         </div>
     )
 }
