bd5a006defad85d839cd445a179b1e327d6ad6b5
Prashanth-Reddy19 <kotha.prashanth02>
2023-01-05 17:39:04 +0530

    Add event listner

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,28 @@
+
 import ExpenseDate from './ExpenseDate';
-import './ExpenseItem.css';
+import './Expenseitem.css';
 
 function ExpenseItem(props) {
 
+    const clickHandler= () =>{
+        console.log('Deleted');
+    }
+    
 
     return (
         <div className='expense-item'>
             <ExpenseDate date={props.date} />
+           
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Delete Expense</button>
+            
+           
+        
         </div>
-    );
+    )
 }
 
-export default ExpenseItem;
+export default ExpenseItem
