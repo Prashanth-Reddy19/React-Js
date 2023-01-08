5766bb945c8590d83ff4ca0666924d57a0656f38
Prashanth-Reddy19 <kotha.prashanth02>
2023-01-08 13:00:10 +0530

    Filter feature

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -7,15 +7,19 @@ import ExpensesFilter from './NewExpense/ExpensesFilter';
 
 
 function Expenses(props) {
-    const [filteredYear, setFilteredYear] = useState(2020)
+    const [filteredYear, setFilteredYear] = useState('2020')
 
     const filetrChangeHandler = selectedYear => {
         setFilteredYear(selectedYear) 
     }
+
+    const filteredExpnses = props.items.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear;
+      });
     return (
         <div className='expenses'>
             <ExpensesFilter selected={filteredYear} onChangeFilter={filetrChangeHandler} />
-            {props.items.map((expense) => (
+            {filteredExpnses.map((expense) => (
                 <ExpenseItem
                 key={expense.id}
                 title={expense.title}
